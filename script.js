9afdfe163c1333c3e82532c6deeb0bbfede83412
irisvdboomen <iris_v.d.boomen@hotmail.com>
2022-05-28 13:33:48 +0200

    more styling and saunakoning project

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function toggleMenu(){
     const menuToggle = document.querySelector('.toggle');
     const navbar = document.querySelector('.navbar');
-    menuToggle.classList.toggle('active')
-    navbar.classList.toggle('active')
+    menuToggle.classList.toggle('active');
+    navbar.classList.toggle('active');
 }
